cca683bd9c850e4f3c50d06fb4db0dc29407d3e7
Samuel Elias <samuel.elias@ibm.com>
2025-11-15 18:41:56 -0300

    Start Project EXPO

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 bg-blue-600 items-center justify-center">
+      <Text className="text-white text-2xl font-bold">
+        NativeWind Funcionando! 🚀
+      </Text>
+    </View>
+  );
+}
